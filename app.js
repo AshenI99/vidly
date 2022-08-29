be7e6dd96d2bfff6246e9ed95fa8bf397d421d9b
AshenI99 <asheniranda99@gmail.com>
2022-08-29 15:58:14 +0530

    Refactored app.js

--- app.js
+++ app.js
@@ -1,54 +1,12 @@
-require("express-async-errors");
-require("winston-mongodb");
-const mongoose = require("mongoose");
-const express = require("express");
-const Joi = require("joi");
-const config = require("config");
 const winston = require("winston");
-
-Joi.objectId = require("joi-objectid")(Joi);
-winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost/vidly' }));
-winston.add(new winston.transports.File({ filename: 'logs/example.log' }));
-
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-
-const error = require("./middlewares/error");
-
+const express = require("express");
 const app = express();
 
-winston.exceptions.handle(new winston.transports.File({
-    filename: 'logs/exceptions.log'
-}))
-
-// winston.rejections.handle(new winston.transports.File({
-//     filename: 'logs/exceptions.log'
-// }))
-
-if(!config.get("jwtPrivateKey")){
-    console.error("Fatal Error: jwtPrivateKey is not defined.");
-    process.exit(1);
-}
-
-mongoose.connect('mongodb://localhost/vidly', null, (err)=>{
-    if(err) console.error("Could not connect to MongoDB ", err.message);
-    else console.log("Connected to MongoDB")
-})
-
-app.use(express.json());
-
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=> console.log(`Listening on port ${port}...`))
+app.listen(port, ()=> winston.info(`Listening on port ${port}...`))
---
+++ startup/logging.js
@@ -0,0 +1,31 @@
+const winston = require("winston");
+require("winston-mongodb");
+require("express-async-errors");
+
+module.exports = function () {
+
+    // winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost/vidly', level: "info" }));
+    winston.add(new winston.transports.File({ filename: 'logs/example.log' }));
+    winston.add(
+        new winston.transports.Console(
+            {
+                format: winston.format.combine(
+                    winston.format.colorize(),
+                    winston.format.prettyPrint()
+                )
+            }
+        )
+    );
+
+    winston.exceptions.handle(new winston.transports.File({
+        filename: 'logs/exceptions.log'
+    }))
+
+    process.on('unhandledRejection', (ex)=>{
+        throw ex;
+    })
+
+    // winston.rejections.handle(new winston.transports.File({
+    //     filename: 'logs/exceptions.log'
+    // }))
+}
