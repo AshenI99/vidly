a5f220435aaadd4561cfdd8e2c5ffb03d046f513
AshenI99 <asheniranda99@gmail.com>
2022-08-30 22:16:06 +0530

    Added winston db config

--- startup/logging.js
+++ startup/logging.js
@@ -1,10 +1,11 @@
+const config = require("config");
 const winston = require("winston");
 require("winston-mongodb");
 require("express-async-errors");
 
 module.exports = function () {
 
-    // winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost/vidly', level: "info" }));
+    winston.add(new winston.transports.MongoDB({ db: config.get('db'), level: "info" }));
     winston.add(new winston.transports.File({ filename: 'logs/example.log' }));
     winston.add(
         new winston.transports.Console(
